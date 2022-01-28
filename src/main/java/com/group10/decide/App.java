ed51e93b08467f26d8d7b5c8c521bc2904a55bb6
Ludwig Johansson <johanssons.ludwig@gmail.com>
2022-01-28 10:12:52 +0100

    Introducing a project structure with Maven fix #1
    This structure includes a setup to include unit tests with junit aswell

---
+++ src/main/java/com/group10/decide/App.java
@@ -0,0 +1,11 @@
+package com.group10.decide;
+
+public class App {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
