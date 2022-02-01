efca2099d536814abac045a0eb4f9f1afbbfa7e3
Amanda Krohn <akrohn@kth.se>
2022-02-01 13:06:49 +0100

    Creates Vector class, fixes #40
    Creates a Vector class. The vector consists of an underlying array with a generic type so that one is able to create vector objects of
    different types. A vector object can be created with a specified length, or a length and initial vector values.
    
    Setter and getters as well as a string formatting method are exposed.

--- src/main/java/com/group10/decide/Vector.java
+++ src/main/java/com/group10/decide/Vector.java
@@ -1,5 +1,42 @@
 package com.group10.decide;
 
+import java.util.Arrays;
+
 public class Vector<T> {
-    
+
+    private int length;
+    private T [] vector;
+
+    public Vector(int length){
+        this.length = length;
+        this.vector = (T[]) new Object[this.length];
+        for(int i = 0; i < length; i++){
+            this.vector[i] = null;
+        }
+    }
+
+    public Vector(int length, T [] values){
+        this.length = length;
+        this.vector = values;
+    }
+
+    public void setValue(int index, T value){
+        if(index > (this.length - 1) || index < 0) throw new IndexOutOfBoundsException("Index " + index + " out of bounds for vector length " + this.length);
+        this.vector[index] = value;
+    }
+
+    public T getValue(int index){
+        if(index > (this.length - 1) || index < 0) throw new IndexOutOfBoundsException("Index " + index + " out of bounds for vector length " + this.length);
+        return this.vector[index];
+    }
+
+    public T [] getValues(){
+        return this.vector;
+    }
+
+    @Override
+    public String toString(){
+        return Arrays.toString(this.vector);
+    }
+
 }
