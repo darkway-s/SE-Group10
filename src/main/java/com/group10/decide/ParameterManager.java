c49ac5cfd1364223d38894511510ca6a77642301
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2022-02-01 14:42:54 +0100

    Fix compilation error, fix #22

--- src/main/java/com/group10/decide/ParameterManager.java
+++ src/main/java/com/group10/decide/ParameterManager.java
@@ -52,13 +52,26 @@ public class ParameterManager {
         return licParameter;
     }
 
-    private readPoints(Scanner scanner) {
+    private void readPoints(Scanner scanner) {
+        // Get the number of points in this input
         numPoints = scanner.nextInt();
+        points = new Point[numPoints];
+        // Get the points data
+        for (int i = 0; i < numPoints; ++i) {
+            points[i].setX(scanner.nextInt());
+            points[i].setY(scanner.nextInt());
+        }
     }
 
-    private initFromFile(String inputFilePath) {
-        File inputFile = new File(inputFilePath);
-        Scanner inputReader = new Scanner(inputFile);
+    private void initFromFile(String inputFilePath) {
+        try {
+            File inputFile = new File(inputFilePath);
+            Scanner inputReader = new Scanner(inputFile);
+            readPoints(inputReader);
+        } catch (FileNotFoundException e) {
+            System.out.println("ParameterManager.initFromFile: File not Found");
+            e.printStackTrace();
+        }
 
     }
 
