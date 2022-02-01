5ad1e5c19b2090991b70c450aeb3b5026fb055ea
Ludwig Johansson <johanssons.ludwig@gmail.com>
2022-02-01 13:40:40 +0100

    Added initialization of Matrix to be used for all tests, fix #39

--- src/test/java/com/group10/decide/MatrixTest.java
+++ src/test/java/com/group10/decide/MatrixTest.java
@@ -1,18 +1,30 @@
 package com.group10.decide;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MatrixTest {
+    Matrix<Integer> matrix;
+    /**
+     * Instantiates a new Matrix test.
+     */
+    @BeforeEach
+    public void setUp(){
+        matrix = new Matrix<>(2, 2);
+        matrix.setValue(0, 0, 1);
+        matrix.setValue(0, 1, 2);
+        matrix.setValue(1, 0, 3);
+        matrix.setValue(1, 1, 4);
+    }
     /**
      * Test if getRows() returns the correct number of rows.
      */
     @Test
     @DisplayName("Get number of matrix rows")
     public void getRows() {
-        Matrix<Integer> matrix = new Matrix<>(2, 2);
         assertEquals(2, matrix.getRowLength(), "Matrix rows should be 2");
     }
     /**
@@ -21,7 +33,6 @@ public class MatrixTest {
     @Test
     @DisplayName("Get number of matrix cols")
     public void getCols() {
-        Matrix<Integer> matrix = new Matrix<>(2, 2);
         assertEquals(2, matrix.getColLength(), "Matrix cols should be 2");
     }
     /**
@@ -30,8 +41,6 @@ public class MatrixTest {
     @Test
     @DisplayName("Get matrix value")
     public void getValue() {
-        Matrix<Integer> matrix = new Matrix<>(2, 2);
-        matrix.setValue(0, 0, 1);
         assertEquals(1, matrix.getValue(0, 0), "Matrix value should be 1");
     }
     /**
@@ -40,11 +49,6 @@ public class MatrixTest {
     @Test
     @DisplayName("Get matrix row")
     public void getRow() {
-        Matrix<Integer> matrix = new Matrix<>(2, 2);
-        matrix.setValue(0, 0, 1);
-        matrix.setValue(0, 1, 2);
-        matrix.setValue(1, 0, 3);
-        matrix.setValue(1, 1, 4);
         Object[] row = matrix.getRow(0);
         assertEquals(1, row[0], "Matrix row should be 1");
         assertEquals(2, row[1], "Matrix row should be 2");
@@ -55,11 +59,6 @@ public class MatrixTest {
     @Test
     @DisplayName("Get matrix col")
     public void getCol() {
-        Matrix<Integer> matrix = new Matrix<>(2, 2);
-        matrix.setValue(0, 0, 1);
-        matrix.setValue(0, 1, 2);
-        matrix.setValue(1, 0, 3);
-        matrix.setValue(1, 1, 4);
         Object[] col = matrix.getCol(0);
         assertEquals(1, col[0], "Matrix col should be 1");
         assertEquals(3, col[1], "Matrix col should be 3");
