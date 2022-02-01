9fbaa5b4b341ddad665eeae01002911140b2ed1c
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2022-02-01 14:42:54 +0100

    Complete readPoints function

--- src/main/java/com/group10/decide/ParameterManager.java
+++ src/main/java/com/group10/decide/ParameterManager.java
@@ -12,12 +12,20 @@ public class ParameterManager {
     private Vector<Boolean> preliminaryUnlockingVector;
 
     private readPoints(Scanner scanner) {
+        // Get the number of points in this input
         numPoints = scanner.nextInt();
+        points = new Point[numPoints];
+        // Get the points data
+        for (int i = 0; i < numPoints; ++i) {
+            points[i].setX(scanner.nextInt())
+            points[i].setY(scanner.nextInt())
+        }
     }
 
     private initFromFile(String inputFilePath) {
         File inputFile = new File(inputFilePath);
         Scanner inputReader = new Scanner(inputFile);
+        readPoints(inputReader)
 
     }
 
