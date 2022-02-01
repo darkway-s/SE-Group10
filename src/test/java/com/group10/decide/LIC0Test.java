0c26edd6e8346aac81359f490be4a62ad8ccf42c
darkways <51851705+darkway-s@users.noreply.github.com>
2022-02-01 22:05:09 +0100

    feat #2 Unit test

--- src/test/java/com/group10/decide/LIC0Test.java
+++ src/test/java/com/group10/decide/LIC0Test.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.concurrent.locks.Condition;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -12,43 +14,37 @@ import org.junit.jupiter.api.BeforeEach;
  * Unit test for LIC0.
  */
 public class LIC0Test {
+    String dir;
+    
 
+    @BeforeEach
+    public void setUp() {
+        String root = System.getProperty("user.dir");
+        this.dir = root + "/test-inputs/";
+    }
+
+    /**
+     * Test if LIC0 Passes
+     */
     @Test
     @DisplayName("LIC0Test1")
     public void LIC0Pass() {
-        int NUMPOINTS = 3;
-        Point point1 = new Point(0, 0);
-        Point point2 = new Point(1, 2);
-        Point point3 = new Point(10, 10);
-        Point[] POINTS = {point1, point2, point3};
-        Matrix<Connector> logicalConnectorMatrix = new Matrix<Connector>();
-        Vector<Boolean> preliminaryUnlockingVector = new Vector<Boolean>();
-        LICParameter licParameter = new LICParameter(1.0, 9.3, 0.092, 24.4, 4.0, 3.4, 6.6, 7.0, 1, 3, 79, 4, 8, 56, 9, 11, 10, 3, 9);
-        ParameterManager pm = new ParameterManager(NUMPOINTS, POINTS, licParameter, logicalConnectorMatrix, preliminaryUnlockingVector);
-        ConditionsMetVector cmv = new ConditionsMetVector(15, pm);
-        
+        ParameterManager myPM = new ParameterManager(dir + "test-4p-0.txt");
+        ConditionsMetVector cmv = new ConditionsMetVector(myPM);
         Boolean result = cmv.LIC0();
         assertEquals(Boolean.TRUE, result, "should get True");
-        
     }
 
 
+    /**
+     * Test if LIC0 Unpasses
+     */
     @Test
-    @DisplayName("LIC0Test1")
+    @DisplayName("LIC0Test2")
     public void LIC0Unpass() {
-        int NUMPOINTS = 3;
-        Point point1 = new Point(0, 0);
-        Point point2 = new Point(1, 1);
-        Point point3 = new Point(1, 2);
-        Point[] POINTS = {point1, point2, point3};
-        Matrix<Connector> logicalConnectorMatrix = new Matrix<Connector>();
-        Vector<Boolean> preliminaryUnlockingVector = new Vector<Boolean>();
-        LICParameter licParameter = new LICParameter(5.0, 9.3, 0.092, 24.4, 4.0, 3.4, 6.6, 7.0, 1, 3, 79, 4, 8, 56, 9, 11, 10, 3, 9);
-        ParameterManager pm = new ParameterManager(NUMPOINTS, POINTS, licParameter, logicalConnectorMatrix, preliminaryUnlockingVector);
-        ConditionsMetVector cmv = new ConditionsMetVector(15, pm);
-        
+        ParameterManager myPM = new ParameterManager(dir + "test-10p-allfalse.txt");
+        ConditionsMetVector cmv = new ConditionsMetVector(myPM);
         Boolean result = cmv.LIC0();
         assertEquals(Boolean.FALSE, result, "should get False");
-        
     }
 }
