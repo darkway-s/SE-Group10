34ac58f6f330688c7dcb242c81c3e3d2f098db31
darkways <51851705+darkway-s@users.noreply.github.com>
2022-02-01 13:31:56 +0100

    Implemented LIC0 and tests, feat #2
    fix #2
    create a simple unit test
    improve small parts of ConidtionsMetVector class and ParameterManager class

--- src/main/java/com/group10/decide/ConditionsMetVector.java
+++ src/main/java/com/group10/decide/ConditionsMetVector.java
@@ -5,74 +5,150 @@ public class ConditionsMetVector {
     int length;
     Vector<Boolean> conditionsMetVector;
 
-    public ConditionsMetVector(int length, ParameterManager pm) {
 
+    public ConditionsMetVector(int length, ParameterManager pm) {
+        this.length = length;
+        this.pm = pm;
     }
 
     public void setConditionsVector() {
 
     }
-
+    
+    
+    /** 
+     * There exists at least one set of two consecutive data points
+     *  that are a distance greater than LENGTH1. (0 ≤ LENGTH1)
+     * @return Boolean. True if there exists two consecutive data points, otherwise false
+     */
     public Boolean LIC0() {
+        int siz = this.pm.getNumPoints();
+        Point[] pointsArray =  this.pm.getPoints();
+        double length1 = this.pm.licParameter.getLength1();
+        for(int i = 0; i < siz - 1; i++)
+        {
+            if(pointsArray[i].distance(pointsArray[i+1]) > length1)
+                return Boolean.TRUE;
+        }
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC1() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC2() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC3() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC4() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC5() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC6() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC7() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC8() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC9() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC10() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC11() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC12() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC13() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Boolean
+     */
     public Boolean LIC14() {
         return Boolean.FALSE;
     }
 
+    
+    /** 
+     * @return Vector<Boolean>
+     */
     public Vector<Boolean> getConditionsMetVector() {
         return this.conditionsMetVector;
     }
