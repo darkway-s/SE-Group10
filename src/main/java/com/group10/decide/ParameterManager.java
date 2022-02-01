fef4f25cacb8afd281e1a2b6c8e38d591fc28c6f
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2022-02-01 14:46:57 +0100

    Fix compilation error

--- src/main/java/com/group10/decide/ParameterManager.java
+++ src/main/java/com/group10/decide/ParameterManager.java
@@ -11,21 +11,26 @@ public class ParameterManager {
     private Matrix<Connector> logicalConnectorMatrix;
     private Vector<Boolean> preliminaryUnlockingVector;
 
-    private readPoints(Scanner scanner) {
+    private void readPoints(Scanner scanner) {
         // Get the number of points in this input
         numPoints = scanner.nextInt();
         points = new Point[numPoints];
         // Get the points data
         for (int i = 0; i < numPoints; ++i) {
-            points[i].setX(scanner.nextInt())
-            points[i].setY(scanner.nextInt())
+            points[i].setX(scanner.nextInt());
+            points[i].setY(scanner.nextInt());
         }
     }
 
-    private initFromFile(String inputFilePath) {
-        File inputFile = new File(inputFilePath);
-        Scanner inputReader = new Scanner(inputFile);
-        readPoints(inputReader)
+    private void initFromFile(String inputFilePath) {
+        try {
+            File inputFile = new File(inputFilePath);
+            Scanner inputReader = new Scanner(inputFile);
+            readPoints(inputReader);
+        } catch (FileNotFoundException e) {
+            System.out.println("ParameterManager.initFromFile: File not Found");
+            e.printStackTrace();
+        }
 
     }
 
