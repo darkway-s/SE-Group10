d6e684f63e240e0ff2be833bf40a23f8743519a3
Ludwig Johansson <johanssons.ludwig@gmail.com>
2022-02-02 23:03:52 +0100

    Main method with tesefile

--- src/main/java/com/group10/decide/LaunchInterceptorProgram.java
+++ src/main/java/com/group10/decide/LaunchInterceptorProgram.java
@@ -7,7 +7,8 @@ public class LaunchInterceptorProgram {
 
     public static void main(String[] args) {
         // Create a new ParameterManager object with an input file path as argument
-        ParameterManager pm = new ParameterManager(args[1]);
+        //ParameterManager pm = new ParameterManager(args[1]);
+        ParameterManager pm = new ParameterManager("test-inputs/test-10p-alltrue.txt");
         Decider d = new Decider(pm);
         boolean decision = d.decide();
         if (decision) {
