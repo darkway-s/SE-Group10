f206708ef53c03177429540b4e82de1ab2fe610e
darkways <51851705+darkway-s@users.noreply.github.com>
2022-02-02 22:18:19 +0100

    Implemented LaunchInterceptorProgram, feat #25 (#93)
    * Implemented the main method, feat #25
    
    * Main method with tesefile
    
    * fix cast errors in LIC6 by replacing the usage of Arrays class
    
    replace the usage of Arrays class
    improve the use of local variables
    
    Co-authored-by: Ludwig Johansson <johanssons.ludwig@gmail.com>

--- src/main/java/com/group10/decide/LaunchInterceptorProgram.java
+++ src/main/java/com/group10/decide/LaunchInterceptorProgram.java
@@ -1,7 +1,20 @@
 package com.group10.decide;
 
+/** 
+ * Main class for the program.
+ */
 public class LaunchInterceptorProgram {
 
     public static void main(String[] args) {
+        // Create a new ParameterManager object with an input file path as argument
+        //ParameterManager pm = new ParameterManager(args[1]);
+        ParameterManager pm = new ParameterManager("test-inputs/test-10p-alltrue.txt");
+        Decider d = new Decider(pm);
+        boolean decision = d.decide();
+        if (decision) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
